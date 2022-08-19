96144e4fd7371b414686b892a3b9cb53fbd74776
jaikrishnaverma <jaikrishnaverma>
2022-08-19 11:30:23 +0530

    print hello world class component and arrow function

--- helloworld/src/components/ClassComp.jsx
+++ helloworld/src/components/ClassComp.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class ClassComp extends Component {
     state = {  } 
-    render() { 
+    render=()=> { 
         return (<h1>Hello World!</h1>);
     }
 }
